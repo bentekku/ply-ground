a02ab7378c6969d9424b53e2ccd6bf32d50f8c4a
Shadab Khan <mdshadab21july@gmail.com>
2024-01-25 19:14:24 +0530

    add endpoint redirect to 'slug' of the game card

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { game } from "../types/game.types";
+
+type SingleGamePageProps = game;
+
+const SingleGamePage = (props: SingleGamePageProps) => {
+  return <div>SingleGamePage</div>;
+};
+
+export default SingleGamePage;
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,5 +1,6 @@
 import { game } from "@/app/types/game.types";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React from "react";
 import { BiPlus, BiSolidStar } from "react-icons/bi";
 
@@ -11,7 +12,11 @@ const Card = ({
   background_image,
   rating,
   ratings_count,
+  slug,
 }: CardProps) => {
+  // INFO: Router is used to redirect
+  const router = useRouter();
+
   // NOTE: Converting `release` to a human readable format
   const releasedDate = new Date(released);
   const humanReadableDate = releasedDate.toLocaleDateString("en-US", {
@@ -21,25 +26,35 @@ const Card = ({
   });
   // console.log(humanReadableDate);
 
+  const openGamePage = (arg_slug: string) => {
+    router.push(`/${arg_slug}`);
+  };
+
+  const toggleAddToUserCollection = () => {};
+
   return (
-    <div className="group transition-all relative border border-white/10 flex flex-col w-[18rem] h-[20rem] md:w-[24rem] md:h-[18rem] lg:w-[28rem] lg:h-[20rem] rounded-xl overflow-clip">
+    <div
+      onClick={() => openGamePage(slug)}
+      className="group transition-all relative border border-white/10 flex flex-col w-[18rem] h-[20rem] md:w-[24rem] md:h-[18rem] lg:w-[28rem] lg:h-[20rem] rounded-xl overflow-clip cursor-pointer"
+    >
       {/* Add to user-collection with status of the game */}
       <Image
         src={background_image}
         alt={name}
         fill
         className="object-cover"
-        priority
+        // priority
+        loading="eager"
       />
 
       {/* Add to user-collection */}
-      <div className="absolute md:group-hover:block md:hidden right-4 top-4 p-4 bg-white text-black flex items-center justify-center text-center font-medium text-xl rounded-full shadow-black/50 shadow-lg cursor-pointer">
+      <div className="absolute md:group-hover:block md:hidden right-4 top-4 p-4 bg-white text-black flex items-center justify-center text-center font-medium text-xl rounded-full shadow-black/50 shadow-lg border border-white/40 cursor-pointer hover:scale-110 focus:scale-110 outline-none transition">
         +
       </div>
 
-      <div className="absolute flex justify-between items-start bg-black/60 w-full px-4 py-5 bottom-0">
+      <div className="absolute flex justify-between items-start bg-black/60 w-full px-4 py-5 bottom-0 ">
         {/* left */}
-        <div>
+        <div className="">
           <p className="font-bold text-white/80 text-lg justify-self-start mb-1">
             {name}
           </p>

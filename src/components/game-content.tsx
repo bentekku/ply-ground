2ba8d431037db41496862e52a87efd33843bbc29
Shadab Khan <mdshadab21july@gmail.com>
2024-01-30 19:39:52 +0530

    tweaked game-detail

--- src/components/game-content.tsx
+++ src/components/game-content.tsx
@@ -16,16 +16,16 @@ const GameContent = ({ specificGame }: GameContentProps) => {
   return (
     <>
       <section className="w-full px-4 py-6 md:px-10 md:py-12 lg:px-13 lg:py-15">
-        <h1 className="text-4xl font-bold underline underline-offset-8 mb-8">
+        <h1 className="text-4xl font-bold underline underline-offset-8 mb-8 mx-auto">
           {specificGame.name}
         </h1>
-        <p className="text-sm text-white/70 px-4 leading-7 mb-4 w-4/5">
+        <p className="text-[1rem] md:text-[1.2rem] md:leading-[2rem] text-white/75 px-4 leading-7 mb-4 mx-auto w-full lg:w-4/5">
           {newDescr}
         </p>
 
         {/* Release date and Rating */}
-        <div className="mb-12">
-          <p className="text-sm text-white/70 px-4 leading-6 mb-4 inline-flex gap-10 flex-wrap">
+        <div className="mb-12 mx-auto w-full lg:w-4/5">
+          <p className="text-[.9rem] md:text-[1rem] md:leading-[1.85rem] text-white/55 px-4 leading-6 mb-4 inline-flex gap-10 flex-wrap">
             <span className="inline-flex items-center gap-2">
               <BiCalendar /> {humanReadableDate}
             </span>
